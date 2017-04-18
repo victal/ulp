f7638020aedf1ebc2e9ff7a0aa09d1a3cf1285e4
Guilherme Victal <guilhermevictal em gmail ponto com>
2017-04-17 21:42:17 -0300

    Fix install from pip since we don't package the readme

--- setup.py
+++ setup.py
@@ -3,9 +3,14 @@
 
 from setuptools import setup
 
+
 def get_description():
-      with open("README.md") as f:
+    try:
+        with open("README.md") as f:
             return f.read()
+    except IOError:
+        return ""
+
 
 setup(name='ulp',
       version='1.0.1',
@@ -19,28 +24,28 @@ setup(name='ulp',
       requires=['pyperclip', 'urwid'],
       scripts=['scripts/ulp'],
       entry_points={
-         'console_scripts':[
-            'ulp_extract=ulp.urlextract:main',
-            'ulp_tui=ulp.tui:main'
-            ]
-         },
+          'console_scripts': [
+              'ulp_extract=ulp.urlextract:main',
+              'ulp_tui=ulp.tui:main'
+          ]
+      },
       classifiers=[
-         # How mature is this project? Common values are
-         #   3 - Alpha
-         #   4 - Beta
-         #   5 - Production/Stable
-         'Development Status :: 3 - Alpha',
+          # How mature is this project? Common values are
+          #   3 - Alpha
+          #   4 - Beta
+          #   5 - Production/Stable
+          'Development Status :: 3 - Alpha',
 
-         # Indicate who your project is intended for
-         'Intended Audience :: Developers',
-         'Topic :: Software Development :: Build Tools',
+          # Indicate who your project is intended for
+          'Intended Audience :: Developers',
+          'Topic :: Software Development :: Build Tools',
 
-         # Pick your license as you wish (should match "license" above)
-         'License :: OSI Approved :: MIT License',
+          # Pick your license as you wish (should match "license" above)
+          'License :: OSI Approved :: MIT License',
 
-         # Specify the Python versions you support here. In particular, ensure
-         # that you indicate whether you support Python 2, Python 3 or both.
-         'Programming Language :: Python :: 3',
-         'Programming Language :: Python :: 3.5',
-         ]
+          # Specify the Python versions you support here. In particular, ensure
+          # that you indicate whether you support Python 2, Python 3 or both.
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.5',
+      ]
       )
