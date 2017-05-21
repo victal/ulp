29b5d823f8eedc5ca68dd2312c2ca98b3eac6fa2
Guilherme Victal <guilhermevictal em gmail ponto com>
2017-05-21 12:03:16 -0300

    Bump version to 1.1.1

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def get_description():
 
 
 setup(name='ulp',
-      version='1.1.0',
+      version='1.1.1',
       author='Guilherme Victal',
       author_email='guilherme@victal.eti.br',
       url='https://github.com/victal/ulp',
