bf5b9df5e53f65f9c59648302af3ed80a4e9a2eb
Guilherme Victal <guilhermevictal em gmail ponto com>
2018-07-30 22:25:27 -0300

    Add -o option to automatically open urls

--- ulp/tui.py
+++ ulp/tui.py
@@ -1,19 +1,26 @@
 #!/usr/bin/env python
 # coding=utf-8
 import sys
+import subprocess
 
 from ulp.main import Interface
 from ulp.urlextract import read_inputfile
+from argparse import ArgumentParser
 
 
 def main():
+    parser = ArgumentParser()
+    parser.add_argument('-o', '--open', dest='open', action='store_true', help='Automatically opens the URL in a browser when only one URL is found')
+    args = parser.parse_args()
     choices = read_inputfile()
     if len(choices) == 0:
         print("-" * 44, file=sys.stderr)
         print("No links found in provided input, exiting...")
         sys.exit(1)
-
-    Interface(choices).run()
+    if len(choices) == 1 and args.open:
+        subprocess.call(['x-www-browser', choices[0]])
+    else:
+        Interface(choices).run()
 
 
 if __name__ == '__main__':
