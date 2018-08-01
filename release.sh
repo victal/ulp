fcf10a121c0ba5a5ae1b2f04755fec168a112d2a
Guilherme Victal <guilhermevictal em gmail ponto com>
2018-07-31 21:53:22 -0300

    Update release script

--- release.sh
+++ release.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-PYTHON=$(which python)
-TWINE=$(which twine)
+PIPENV=$(which pipenv)
+
+if [ -z "$PIPENV" ]
+    echo "pipenv not found. Install pipenv with 'pip3 install --user pipenv' before running this script"
+    exit 1
+fi
+
+$PIPENV install --dev
 
 CURRENT_VERSION="$(python setup.py --version)"
 NEXT_VERSION="$1"
+if [ -z "$NEXT_VERSION" ]
+then
+    echo "Next version not provided, releasing a new bugfix version"
+    NEXT_VERSION="$(echo $CURRENT_VERSION | awk 'BEGIN{FS=OFS="."}{$NF++;print;}')"
+fi
 
 #Cleanup
 rm -rf build/ dist/
@@ -16,10 +27,10 @@ sed -i "s/'$CURRENT_VERSION'/'$NEXT_VERSION'/g" setup.py
 python setup.py sdist bdist_wheel
 
 #Deploy artifacts
-#twine upload dist/*
+twine upload dist/*
 
 #Create git tag
 git add setup.py
 git ci -m "Bump version to $NEXT_VERSION for release"
 git tag -a -m "Release $NEXT_VERSION" "$NEXT_VERSION"
-#git push --tags
+git push --tags
